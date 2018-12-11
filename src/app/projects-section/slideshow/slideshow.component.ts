b7c620aa4d1ca7baad69d7f1c185cde10a938e72
Joseph Joyce <joej326@gmail.com>
2018-12-11 11:04:20 -0500

    chamnged 1 or -1 to number

--- src/app/projects-section/slideshow/slideshow.component.ts
+++ src/app/projects-section/slideshow/slideshow.component.ts
@@ -45,10 +45,10 @@ export class SlideshowComponent implements OnInit {
   ngOnInit() {
   }
 
-  changeSlide(direction: 1 | -1) {
+  changeSlide(direction: number) {
     if (this.currentSlide === 0 && direction === -1) {
-      this.currentSlide = this.slides.length-1;
-    } else if (this.currentSlide === this.slides.length-1 && direction === 1) {
+      this.currentSlide = this.slides.length - 1;
+    } else if (this.currentSlide === this.slides.length - 1 && direction === 1) {
       this.currentSlide = 0;
     } else {
       this.currentSlide += direction;
