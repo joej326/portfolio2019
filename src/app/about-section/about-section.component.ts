d421ffae28c6347ccbb9f603efaf99e8fdd7bb0c
Joseph Joyce <joej326@gmail.com>
2019-04-19 14:21:22 -0400

    tweaked the about me section wording

--- src/app/about-section/about-section.component.ts
+++ src/app/about-section/about-section.component.ts
@@ -10,18 +10,18 @@ import { ScrollMasterModel } from '../scroll-master-model';
 export class AboutSectionComponent implements OnInit {
 
   scrollInfoObj: ScrollMasterModel;
-  aboutMe = `I was born and raised in California.
+  aboutMe = `I was born and raised in the small and little-known town of Novato, California.
 
-  At the age of 20  I received a job offer in Melbourne, FL to work my first professional job
+  At the age of 20  I accepted a job offer in Melbourne, Florida to work my first professional job
   as a Software Developer.
 
   The move was a big one, considering I was leaving behind my friends and family to reside in
-  Florida where I didn’t know anyone. The journey has proved invaluable to me as it has taught
-  me how to live independently and make meaningful connections in a world where people are
-  becoming increasingly socially isolated.
+  Florida where I didn’t know anyone. The journey has proven invaluable to me as it taught me how
+  to live independently and meet interesting people in a world where people are becoming
+  increasingly socially isolated.
 
-  A passion of mine besides developing software is philosophy, more particularly existentialism.
-  Conversing about how one should live a meaningful life makes me giddy simply at the thought of it!
+  A passion of mine other than developing software is philosophy. More particularly existentialism.
+  Contemplating how we can live meaningful lives makes me giddy simply thinking about it!
 
   My hobbies include:
   coding, reading, writing, longboarding, rollerblading, going to the gym, and teaching.`;
